b677da5d8c43c53e267ad9f2049a3ca7d77f2b67
Matias Basilotta <matiasbasilotta@gmail.com>
2023-08-01 23:30:03 -0300

    Adam agregado

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,11 +5,11 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./adamHead/adamHead.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={0.5} groundColor='black' />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -18,12 +18,12 @@ const Computers = ({ isMobile }) => {
         castShadow
         shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
+      <pointLight intensity={1.5} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 0.7 : 2.5}
+        position={isMobile ? [0, -3, -2.2] : [-5, -3, 2]}
+        rotation={[0.1, -1.2, 0.2]}
       />
     </mesh>
   );
